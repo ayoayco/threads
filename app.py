dc11618b3bcdad330fe437d7165118d3ca19337f
Ayo Ayco <ayo@ayco.io>
2024-04-19 19:23:47 +0200

    feat: return cleaned up thread status and descendants

---
+++ app.py
@@ -0,0 +1,36 @@
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+server = 'https://social.ayco.io'
+thread_ids = ['112294405672971916']
+
+@app.route('/')
+def home():
+    threads = []
+    for id in thread_ids:
+        status = requests.get(server + '/api/v1/statuses/' + id ).json()
+        author = status['account']['acct']
+        context = requests.get(server + '/api/v1/statuses/' + id + '/context').json()
+        descendants = []
+        for reply in context['descendants']:
+            if reply['account']['acct'] == author:
+                descendants.append(clean_status(reply))
+        status['descendants'] = descendants
+        threads.append(clean_status(status))
+    return threads
+
+def clean_status(status):
+    clean = {}
+    clean['content'] = status['content']
+    clean['created_at'] = status['created_at']
+    clean['url'] = status['url']
+    if 'descendants' in status:
+        descendants = status['descendants']
+        clean['descendants_count'] = len(descendants)
+        clean['descendants'] = descendants
+    return clean
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
