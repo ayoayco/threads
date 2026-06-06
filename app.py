cd1d2821a44b61710d7e9a5fc45d3b6cab5a8b48
Ayo <ayo@ayco.io>
2026-06-06 15:35:56 +0200

    chore: indicate watching extra files;

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from .threads import threads
 from .cache import cache
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 cache.init_app(app, config={'CACHE_TYPE': 'SimpleCache'})
 app.register_blueprint(threads, url_prefix='/')
 app.config.from_file("config.json", load=json.load)
@@ -16,7 +15,7 @@ filtered = filter(lambda file: not file.startswith('.'), template_files)
 template_files = list(filtered)
 template_files = ['./templates/{0}'.format(file) for file in template_files]
 extra_files = ':'.join(template_files)
-print(" * Watching: ")
+print(" * Watching extra files: ")
 for file in template_files:
     print(f"    - {file}")
 os.environ['FLASK_RUN_EXTRA_FILES'] = extra_files
